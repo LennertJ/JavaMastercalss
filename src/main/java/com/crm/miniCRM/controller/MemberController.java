70478d6ee4c3728f0305b6983dd5603391210098
Lennert Jans | Opleiding Elmos <lennert.jans@elmos.be>
2022-09-01 10:12:10 +0200

    memberlistpage

--- src/main/java/com/crm/miniCRM/controller/MemberController.java
+++ src/main/java/com/crm/miniCRM/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.crm.miniCRM.dto.MemberDto;
 import com.crm.miniCRM.dto.PersonDto;
 import com.crm.miniCRM.model.Member;
 import com.crm.miniCRM.model.Person;
+import com.crm.miniCRM.model.persistence.helpers.MemberID;
 import com.crm.miniCRM.model.persistence.interfaces.MemberRepository;
 import com.crm.miniCRM.model.persistence.interfaces.PersonRepository;
 import org.springframework.stereotype.Controller;
@@ -17,22 +18,39 @@ import java.util.List;
 @Controller
 @RequestMapping(value = "/members")
 public class MemberController {
-    private MemberRepository memberRepository;
+    private final MemberRepository memberService;
+    private final PersonRepository personService;
 
-    public MemberController(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
+    public MemberController(MemberRepository memberService, PersonRepository personService) {
+        this.memberService = memberService;
+        this.personService = personService;
     }
 
     @GetMapping
     public String getmembers(Model model) {
-        Iterable<Member> members = memberRepository.findAll();
+        Iterable<Member> members = memberService.findAll();
         List<MemberDto> memberDtos = new ArrayList<>();
-        members.forEach(m -> memberDtos.add(convertToDto(m)));
+        members.forEach(m-> {
+            boolean communityExists = false;
+            MemberID memberID = m.getId();
+            Long personId = memberID.getPerson_ID();
+            Person person = this.personService.findById(personId).orElse(null);
+            if(person ==null){
+                return;
+            }
+            Long communityId = memberID.getCommunity_ID();
+            for (MemberDto memberdto: memberDtos) {
+                if(memberdto.getCommunityId()==communityId){
+                    memberdto.addPersonToCommunity(person);
+                    communityExists = true;
+                }
+            }
+            if(!communityExists){
+                memberDtos.add(new MemberDto(communityId, person, m.getSince(),m.getUntil()));
+            }
+
+        });
         model.addAttribute("members", memberDtos);
         return "members";
-    }
-
-    protected MemberDto convertToDto(Member entity) {
-        return new MemberDto(entity.getId(), entity.getSince(),entity.getUntil());
     }
 }
